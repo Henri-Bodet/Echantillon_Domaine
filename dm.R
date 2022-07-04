8d0b1da869964227852e377ebd31fea700d4735e
Henri Bodet <henri_bodet@orange.fr>
2022-07-04 08:42:22 +0200

    initialise dm

---
+++ dm.R
@@ -0,0 +1,4 @@
+#### distribution de la taille de l'échantillon tombant dans un domaine
+### cas d'un sAS
+### variables de contexte : N (pop),n(échantillon),M(domaine)
+dm <- function(x,N=,n=,M=){}
